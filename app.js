7befd72afa62c5f4fd673ddcb75de2afce8f605c
Orest <Orestduda@gmail.com>
2021-07-11 16:11:36 +0300

    hw4

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
+const { errorHandler } = require('./services/errorHandler')
 
 const app = express()
 
@@ -13,6 +15,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter)
+app.use(errorHandler)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const errors = require('../services/errors')
+const { User } = require('../db/usersModel')
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const [token] = req.headers.authorization.split(' ')
+    if (!token) {
+      next(new errors.NotAuthorizedError('Not authorized'))
+    }
+    const user = jwt.decode(token, process.env.JWT_SECRET)
+    const isUserExist = await User.findOne({ _id: user._id })
+    if (!isUserExist || isUserExist.token !== token) {
+      next(new errors.NotAuthorizedError('Not authorized'))
+    }
+    req.user = user
+    next()
+  } catch (err) {
+    next(new errors.NotAuthorizedError('Not authorized'))
+  }
+}
+
+module.exports = {
+  authMiddleware
+}
